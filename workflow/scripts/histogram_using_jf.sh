a2fc11801a97b1d9f70ffda4bac2ec5153d7fa62
Karel Břinda <karel.brinda@gmail.com>
2021-07-27 17:49:55 -0400

    Add script to calculate histogram using JellyFish

---
+++ workflow/scripts/histogram_using_jf.sh
@@ -0,0 +1,43 @@
+#! /usr/bin/env bash
+
+set -e
+set -o pipefail
+set -u
+#set -f
+
+readonly PROGNAME=$(basename $0)
+readonly PROGDIR=$(dirname $0)
+readonly -a ARGS=("$@")
+readonly NARGS="$#"
+
+if [[ $NARGS -ne 1 ]]; then
+	>&2 echo "usage: $PROGNAME input.fa"
+	exit 1
+fi
+
+jellyfish --version 2>&1 > jellyfish_version.txt
+
+x="$1"
+y=$(mktemp -d)/count.jf
+
+>&2 echo "Input file: $x"
+>&2 echo "Counting file: $y"
+
+threads=7
+
+jellyfish count \
+	--threads $threads \
+	--canonical \
+	--mer-len 31 \
+	--size 20M \
+	--output "$y" \
+	"$x"
+
+printf 'freq\tkmers\n'
+jellyfish histo \
+	--threads $threads \
+	--high 1000000 \
+	"$y" \
+	| perl -pe 's/ /\t/g'
+
+rm -f "$y"
