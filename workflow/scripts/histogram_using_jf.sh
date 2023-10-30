701f91d84b95c1b1fd9e660ec169e1f553e4db4f
Karel Břinda <karel.brinda@gmail.com>
2023-10-30 23:15:21 +0100

    Improve configurability of histogram_using_jf.sh

--- workflow/scripts/histogram_using_jf.sh
+++ workflow/scripts/histogram_using_jf.sh
@@ -5,8 +5,31 @@ set -o pipefail
 set -u
 #set -f
 
-while getopts ":k:t:" opt; do
+
+default_s="20M"
+default_k=31
+default_t=$(nproc --all)
+
+s="$default_s"
+t="$default_t"
+k="$default_k"
+
+function help_msg {
+	>&2 echo
+	>&2 echo "Program:  $PROGNAME - get k-mer histogram using JellyFish"
+	>&2 echo
+	>&2 echo "Usage:    $PROGNAME [-s str] [-t int] [-k int] input.fa"
+	>&2 echo
+	>&2 echo "Options:  -s STR   initial hash size [$default_s]"
+	>&2 echo "          -t INT   number of threads [$default_t]"
+	>&2 echo "          -k INT   k-mer length [$default_k]"
+	>&2 echo
+	exit 1
+}
+
+while getopts ":s:k:t:" opt; do
   case $opt in
+    s) s="$OPTARG" ;;
     k) k="$OPTARG" ;;
     t) t="$OPTARG" ;;
   esac
@@ -21,8 +44,7 @@ readonly NARGS="$#"
 
 
 if [[ $NARGS -ne 1 ]]; then
-	>&2 echo "usage: $PROGNAME -t {threads} -k {kmer_length} input.fa"
-	exit 1
+    help_msg
 fi
 
 x="$1"
@@ -34,6 +56,11 @@ y="$(mktemp -d)/count.jf"
 
 (
 set +u
+if [[ -z "$s" ]]; then
+	echo "Error: initial hash table size not provided (-s)" 1>&2
+    exit 1
+fi
+
 if [[ -z "$k" ]]; then
 	echo "Error: k-mer length not provided (-k)" 1>&2
     exit 1
@@ -45,11 +72,13 @@ if [[ -z "$t" ]]; then
 fi
 )
 
+echo "Parameters: s=$s k=$k t=$t" 1>&2
+
 jellyfish count \
 	--threads "$t" \
 	--canonical \
 	--mer-len "$k" \
-	--size 20M \
+	--size "$s" \
 	--output "$y" \
 	"$x"
 
@@ -61,3 +90,4 @@ jellyfish histo \
 	| perl -pe 's/ /\t/g'
 
 rm -f "$y"
+
